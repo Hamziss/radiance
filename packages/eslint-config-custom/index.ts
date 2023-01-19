906638980af521f724137a23fb47b76fdcbac7c0
Hamza Chebbah <hamzachebbah9999@gmail.com>
2023-01-19 18:16:24 +0100

    config: add eslint

--- packages/eslint-config-custom/index.ts
+++ packages/eslint-config-custom/index.ts
@@ -1,7 +1,16 @@
 module.exports = {
-    extends: ["next", "next/babel", "next/core-web-vitals", "turbo", "prettier"],
-    rules: {
-        "@next/next/no-html-link-for-pages": "off",
-        "react/jsx-key": "off",
-    },
-};
+	extends: [
+		"plugin:@typescript-eslint/recommended",
+		"next",
+		"next/babel",
+		"next/core-web-vitals",
+		"turbo",
+		"prettier",
+	],
+	rules: {
+		"@next/next/no-html-link-for-pages": "off",
+		"react/jsx-key": "off",
+	},
+	plugins: ["@typescript-eslint"],
+	parser: "@typescript-eslint/parser",
+}
