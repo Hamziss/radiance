bcd5d0016b7f9c9074d0d9abf27351fcfbab68a5
Hamza Chebbah <hamzachebbah9999@gmail.com>
2023-01-22 18:35:12 +0100

    config: added base tailwind config

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { Button } from "ui"
 
 export default function Web() {
-	console.log("Button")
 	return (
 		<div>
 			<h1>Web</h1>
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,3 +1,7 @@
 export const Button = () => {
-  return <button>Boop</button>;
-};
+	return (
+		<button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+			Boop
+		</button>
+	)
+}
