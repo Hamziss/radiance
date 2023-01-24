396dccc7308427c4a8f4d19aa4b536e37c76c4ed
Hamza Chebbah <hamzachebbah9999@gmail.com>
2023-01-24 14:14:13 +0100

    test ci

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -9,7 +9,7 @@ module.exports = {
         "@next/next/no-html-link-for-pages": "off",
         "react/jsx-key": "off",
         "@typescript-eslint/no-var-requires": "off",
-        "no-console": "error",
+        // "no-console": "error",
 
 
     },
