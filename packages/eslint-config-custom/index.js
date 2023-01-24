9aa30f3e8323ffc0c3abce2b5ae5e8b60cf2fc0a
Hamza Chebbah <hamzachebbah9999@gmail.com>
2023-01-24 15:19:51 +0100

    chore: uncomment cmd

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -9,9 +9,6 @@ module.exports = {
         "@next/next/no-html-link-for-pages": "off",
         "react/jsx-key": "off",
         "@typescript-eslint/no-var-requires": "off",
-        "no-console": "error",
-
-
     },
     plugins: ["@typescript-eslint"],
     parser: "@typescript-eslint/parser",
