8885ee8ed0e45af849d6108e3707c45bcf617ee0
Hamza Chebbah <hamzachebbah9999@gmail.com>
2023-02-02 16:43:33 +0100

    fix: ts next config

--- apps/www/src/pages/index.tsx
+++ apps/www/src/pages/index.tsx
@@ -41,7 +41,7 @@ const AuthShowcase: React.FC = () => {
 
 	const { data: secretMessage } = api.example.getSecretMessage.useQuery(
 		undefined, // no input
-		{ enabled: sessionData?.user !== undefined }
+		{ enabled: sessionData?.user !== undefined },
 	)
 
 	return (
