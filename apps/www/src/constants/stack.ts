4395d0c25152b98e7e8413ef159ed0ab76e88d06
Hamza Chebbah <hamzachebbah9999@gmail.com>
2023-02-05 21:32:56 +0100

    feat: added stack

--- apps/www/src/constants/stack.ts
+++ apps/www/src/constants/stack.ts
@@ -3,6 +3,7 @@ import next from "../../public/stack/next.png"
 import react from "../../public/stack/react.svg"
 import svelte from "../../public/stack/svelte.png"
 import tailwind from "../../public/stack/tailwind.png"
+import trpc from "../../public/stack/trpc.svg"
 import ts from "../../public/stack/ts.png"
 import vue from "../../public/stack/vue.png"
 
@@ -26,6 +27,11 @@ export const stack = [
 		name: "Vue",
 		tooltip: "VueJS",
 		icon: vue,
+	},
+	{
+		name: "tRPC",
+		tooltip: "tRPC",
+		icon: trpc,
 	},
 	{
 		name: "Astro",
