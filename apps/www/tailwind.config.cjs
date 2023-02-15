3746b4c93552e88101f1a09e506e009c9bd11f0f
Hamza Chebbah <hamzachebbah9999@gmail.com>
2023-02-15 23:30:14 +0100

    add animation effect

--- apps/www/tailwind.config.cjs
+++ apps/www/tailwind.config.cjs
@@ -1,4 +1,24 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
     presets: [require("@acme/tailwind-config")],
+    theme: {
+        extend: {
+            animation: {
+                tilt: 'tilt 10s infinite linear',
+            },
+            keyframes: {
+                tilt: {
+                    '0%, 50%, 100%': {
+                        transform: 'rotate(0deg)',
+                    },
+                    '25%': {
+                        transform: 'rotate(1.2deg)',
+                    },
+                    '75%': {
+                        transform: 'rotate(-1.2deg)',
+                    },
+                },
+            },
+        },
+    }
 };
