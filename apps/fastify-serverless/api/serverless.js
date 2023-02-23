e2851eca72520a2952243a97aac24154858168e0
Hamza Chebbah <hamzachebbah9999@gmail.com>
2023-02-23 09:53:05 +0100

    edge serverless

--- apps/fastify-serverless/api/serverless.js
+++ apps/fastify-serverless/api/serverless.js
@@ -17,10 +17,13 @@ const schema = {
 }
 
 app.get('/', schema, function (req, res) {
-  res.send({ message: 'hello from fastify serverless' })
+  res.send({ message: 'hello from fastify serverless edge' })
 })
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req, res) => {
   await app.ready();
   app.server.emit('request', req, res);
+}
+export const config = {
+  runtime: 'edge',
 }
