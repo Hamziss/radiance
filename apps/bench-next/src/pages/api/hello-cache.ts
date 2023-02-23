20cb690c35e7427543b23be7be736425e7e0a928
Hamza Chebbah <hamzachebbah9999@gmail.com>
2023-02-23 09:17:11 +0100

    added edge to cache route

--- apps/bench-next/src/pages/api/hello-cache.ts
+++ apps/bench-next/src/pages/api/hello-cache.ts
@@ -1,13 +1,19 @@
-import type { NextApiRequest, NextApiResponse } from "next"
+import type { NextApiRequest } from "next"
 
-type Data = {
-	message: string
+export const config = {
+	runtime: "experimental-edge",
 }
-
-export default function handler(
-	req: NextApiRequest,
-	res: NextApiResponse<Data>,
-) {
-	res.setHeader("Cache-Control", "s-maxage=86400")
-	res.status(200).json({ message: "Hello from NextJS API routes" })
+export default function handler(req: NextApiRequest) {
+	return new Response(
+		JSON.stringify({
+			message: "Hello from NextJS API routes on the Edge!",
+		}),
+		{
+			status: 200,
+			headers: {
+				"content-type": "application/json",
+				"Cache-Control": "s-maxage=86400",
+			},
+		},
+	)
 }
