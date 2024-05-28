06e123074750f25b81648992d7ed695805665997
Hamza <96361868+Hamziss@users.noreply.github.com>
2024-05-28 22:07:51 +0100

    Update hello.ts

--- apps/bench-next/src/pages/api/hello.ts
+++ apps/bench-next/src/pages/api/hello.ts
@@ -5,9 +5,12 @@ type Data = {
 	message: string
 }
 
-export default function handler(
-	req: NextApiRequest,
-	res: NextApiResponse<Data>,
-) {
-	res.status(200).json({ message: "Hello from NextJS API routes" })
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+  delay(400).then(() => {
+    res.status(200).json({
+      message: `This route with id ${req.query.id} is called at ${new Date()}`,
+    });
+  });
 }
